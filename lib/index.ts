f2e67f135d8cfb9ba4c4a8430bde77ed3b22c8c3
James Jansson <jamesjansson@gmail.com>
2019-02-18 01:31:51 +1100

    Spelling

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ import { sha256Digest } from './bichlmeier-sha256';
 export function sha256Plus(message: string) {
   if (typeof message !== 'string') {
     console.log(message);
-    throw new Error('sha256Plus only access inputs of type string');
+    throw new Error('sha256Plus only accepts inputs of type string');
   }
   const hash1 = sha256Digest(message);
   const hash2 = sha256Digest(hash1 + message);
